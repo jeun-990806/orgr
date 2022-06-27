be47accf05d1beb1b188814b1d99f977c143a194
jeun <kjeun990806@gmail.com>
2022-06-27 17:38:04 +0900

    테스트 버튼 삭제

--- lib/screens/quizIntroPage.dart
+++ lib/screens/quizIntroPage.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'quizPage.dart';
-import 'layoutTestPage.dart';
 
 class QuizIntroPage extends StatelessWidget {
   QuizIntroPage() : super();
@@ -16,12 +15,6 @@ class QuizIntroPage extends StatelessWidget {
             ElevatedButton(
               onPressed: () {
                 Navigator.of(context).push(_createRoute());
-              },
-              child: const Text('Start!'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).push(MaterialPageRoute(builder: (context) => LayoutTestPage()));
               },
               child: const Text('Start!'),
             ),
